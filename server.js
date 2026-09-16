ab6d380b0f56c5262b7b108635d9edc3010a9c2d
M F <mf@MacBook-Pro-M.local>
2026-09-16 23:31:53 +0300

    Add floating organic blobs, circles, mentor photo fixes, and vercel static config

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
+app.get('/demo_preview', (req, res) => {
+  res.sendFile(join(__dirname, 'demo_preview.html'));
+});
+
 app.get('*', (req, res) => {
+  if (req.path.includes('.') && !req.path.endsWith('.html')) {
+    return res.status(404).send('Not found');
+  }
   res.sendFile(join(__dirname, 'index.html'));
 });
 
